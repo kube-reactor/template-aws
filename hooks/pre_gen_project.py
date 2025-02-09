b692234c1a517a5e71386841aa274367aab4e5da
Adrian Webb <adrian@webb.sh>
2025-02-09 17:05:53 -0500

    Adding some temporary debug statements for CI/CD failure debugging purposes.

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -22,7 +22,13 @@ SUCCESS = "\x1b[1;32m [SUCCESS]: "
 """
 
 project_slug = "{{ cookiecutter.project_slug }}"
+print(project_slug)
 if hasattr(project_slug, "isidentifier"):
-    assert project_slug.isidentifier(), "'{}' project slug is not a valid Python identifier.".format(project_slug)
+    assert (
+        project_slug.isidentifier()
+    ), "'{}' project slug is not a valid Python identifier.".format(project_slug)
 
-assert project_slug == project_slug.lower(), "'{}' project slug should be all lowercase".format(project_slug)
+assert (
+    project_slug == project_slug.lower()
+), "'{}' project slug should be all lowercase".format(project_slug)
+print(project_slug)
